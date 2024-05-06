f1bce86875ef90726408dcd52d26055a7b395544
manav <manav18gadhiya@gmail.com>
2024-05-06 16:07:12 +0530

    Basic NFT Done

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {MintBasicNft} from "../script/Interactions.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant SHIBA_INU =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+
+        /*
+        Cannot compare two strings as they are
+        simply an array of bytes and you just
+        cannot compare two different `bytes` values
+        */
+        // assert(expectedName == actualName);
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(SHIBA_INU);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(SHIBA_INU)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
+    }
+
+    function testMintWithScript() public {
+        uint256 startingTokenCount = basicNft.getTokenCounter();
+        MintBasicNft mintBasicNft = new MintBasicNft();
+        mintBasicNft.mintNftOnContract(address(basicNft));
+        assert(basicNft.getTokenCounter() == startingTokenCount + 1);
+    }
+}
