0183be8d4674cdf312cd10ccd18ac572b571eae2
manav <manav18gadhiya@gmail.com>
2024-05-07 22:43:45 +0530

    MoodNFT DONEEEEE

--- test/integrations/BasicNftTest.t.sol
+++ test/integrations/BasicNftTest.t.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
-import {BasicNft} from "../src/BasicNft.sol";
-import {MintBasicNft} from "../script/Interactions.s.sol";
+import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../../src/BasicNft.sol";
+import {MintBasicNft} from "../../script/Interactions.s.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
